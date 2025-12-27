53ae0f39b91030b1b17bd768bb561a1ae714a82c
vruti <ranpariyavruti1@gmail.com>
2025-12-27 13:51:56 +0530

    add implement

--- sum.java
+++ sum.java
@@ -1,9 +1,10 @@
 class Addition{
     public static void main(String[]args){
-        int a=10;
-        int b=20;
+        int a=20;
+        int b=10;
         int sum=a+b;
-
+        int sub=a-b;
         System.out.println("sum ="+sum);
+        System.out.println("sub ="+sub);
     }
 }
